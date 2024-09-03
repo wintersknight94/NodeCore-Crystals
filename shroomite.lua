c836e0392810efc034ec409fe90c7eb89345adba
wintersknight94 <62315647+wintersknight94@users.noreply.github.com>
2024-09-03 06:38:59 -0500

    Shroomite Tools Glow Again

--- shroomite.lua
+++ shroomite.lua
@@ -42,3 +42,18 @@ minetest.override_item(modname.. ":shroomite_lattice",{
 })
 -- ================================================================== --
 
+local function shroomite_tools(item)
+	minetest.override_item(item,{
+		groups = {lux_emit = 1},
+		light_source = 6, glow = 1
+	})
+end
+------------------------------------------------------------------------
+shroomite_tools(modname.. ":tool_pick_shroomite")
+shroomite_tools(modname.. ":tool_spade_shroomite")
+shroomite_tools(modname.. ":tool_hatchet_shroomite")
+shroomite_tools(modname.. ":tool_mallet_shroomite")
+shroomite_tools(modname.. ":adze_shroomite")
+shroomite_tools(modname.. ":mace_shroomite")
+-- ================================================================== --
+
